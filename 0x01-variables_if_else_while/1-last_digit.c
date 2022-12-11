e2fecac1e3598acd5e5c6a1489eaddd91c7359a8
Distinctsy <oluwaseyiomi@gmail.com>
2022-12-11 10:50:59 -0800

    positive

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - annoying
+ *
+ * Reture: 0
+ */
+int main(void)
+{
+	int n;
+	int x;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (x > 5)
+	{
+		printf("and is greater than 5")
+	}
+	if (x==0)
+	{
+		printf("and is op)
+	}
+	return (0);
+}
