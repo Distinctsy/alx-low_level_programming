dc4ab80f6d80583d73bedf349911d53a4cb58cf9
Distinctsy <oluwaseyiomi@gmail.com>
2022-12-12 02:46:40 -0800

    get

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,9 +3,9 @@
 #include <stdio.h>
 
 /**
- * main - annoying
- *
- * Reture: 0
+ * main -> assign a random number to the variable n each time it is executed
+ * and print the last digit of the number stored in the variable n
+ * Reture: Always 0 (Success)
  */
 int main(void)
 {
