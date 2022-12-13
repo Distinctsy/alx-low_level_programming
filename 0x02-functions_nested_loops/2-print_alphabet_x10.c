604ff582f8f6ebb710cf89693efc1aa983b5f14a
Distinctsy <oluwaseyiomi@gmail.com>
2022-12-13 07:28:16 -0800

    ear

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,8 +1,8 @@
 #include "main.h"
-/**
- * print_print_alphabet_x10 - print 10 times the alphabet, in lowercase
- */
 
+/**
+ * print_print_alphabet_x10 - Print 10 times the alphabet, in lowercase.
+ */
 void print_alphabet_x10(void);
 {
 	int count = 0;
@@ -12,6 +12,6 @@ void print_alphabet_x10(void);
 	{
 		for (letter = 'a'; letter <= 'z'; letter++)
 			_putchar(letter);
-		_putchar('/n');
+		_putchar('\n');
 	}
 }
