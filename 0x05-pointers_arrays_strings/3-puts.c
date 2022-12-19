a97fe75d176e8a9bb013134f47b4b5fb782a69f6
Distinctsy <oluwaseyiomi@gmail.com>
2022-12-19 10:49:54 -0800

    3-put

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - print a string into the stdout
+ * @str: the string
+ * Return: string length
+ */
+
+void _puts(char *str)
+{
+	int y = 0;
+
+	while (str[y] != '\0')
+	{
+		_putchar(str[y]);
+		y++;
+	}
+
+}
